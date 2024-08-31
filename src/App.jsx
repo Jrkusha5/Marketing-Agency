6c9f789e08687beb9d4f84a56e902ed2f5788134
Chernet Tafesse <120357658+Jrkusha5@users.noreply.github.com>
2024-08-31 14:04:44 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+   <div className="font-family">
+    <Navbar/>
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { HiOutlineMenuAlt3, HiOutlineX } from "react-icons/hi";
+import { MdOutlineClose } from "react-icons/md";
+
+const Navbar = ()=>{
+
+    const [isOpen, setIsOpen]= useState(false);
+    const [activeItem, setActiveItem]=useState('');
+
+    const toggleMenu =()=>{
+        console.log('menu Clicked')
+        setIsOpen(!isOpen)
+    }
+    const handleMenuClick =(name)=>{
+          setActiveItem(name);
+          setIsOpen(false);
+    }
+
+    const menuItems =[
+        {name: 'About Us', href:'#about'},
+        {name: 'Services', href:'#services'},
+        {name: 'UseCases', href:'#useCases'},
+        {name: 'Team', href:'#team'},
+        {name: 'Testimonials', href:'#testimonials'},
+    ]
+    return(
+        <nav>
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <div className="flex justify-between h-16 items-center
+               ">
+              <div>
+                    <a href="/">
+                        <img src="/logo.png" alt=""  className="h-8 "/>
+                    </a>
+                </div>
+    
+            <div className="space-x-8 hidden md:flex items-center">
+                {
+                    menuItems.map((item,index)=> (
+                        <a className="text-black px-2 hover:bg-primary rounded-sm
+                        hover:underline underline-offset-4 transition-all duration-300"
+                         key={index} href={item.href}>{item.name}</a>
+                    ))
+                }
+                 <a href="#contact" className="border px-4 py-2 border-black hover:bg-primary
+                 hover:border-primary rounded-md transition-all duration-300 ">Request a Quote</a>
+            </div>
+
+            <div className="md:hidden flex items-center">
+                <button 
+                onClick={toggleMenu}
+                className="text-gray-700 hover:text-primary">
+                    {
+                        isOpen ? <MdOutlineClose className="size-6"/> : <HiOutlineMenuAlt3 className="size-6"/>
+                    }
+                    
+                    </button>
+            </div>
+              </div>
+
+            </div>
+
+            {/* mobile menu */}
+            {
+                isOpen && (
+                    <div className="md:hidden bg-white shadow-md">
+                        <div className="px-4 pt-2 pb-3 space-y-2 sm:px-3">
+
+                        {
+                    menuItems.map((item,index)=> (
+                        <a className="text-black block px-2 hover:bg-primary rounded-sm
+                        hover:underline underline-offset-4 transition-all duration-300"
+                         key={index} href={item.href}>{item.name}</a>
+                    ))
+                }
+                 <a href="#contact" className="block border px-4 py-2 border-black hover:bg-primary
+                 hover:border-primary rounded-md transition-all duration-300 ">Request a Quote</a>
+                        </div>
+                    </div>
+                )
+            }
+        </nav>
+    )
+}
+export default Navbar;
