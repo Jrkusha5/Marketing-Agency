7169c1de7000fc75b47cd92ca43deeecdfe83ae0
Chernet Tafesse <120357658+Jrkusha5@users.noreply.github.com>
2024-09-02 13:54:41 -0700

    first commit

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,16 +1,36 @@
 import React from 'react'
+import { servicesData } from '../utils/services'
+import LinkIcon from '../assets/link-icon.png'
 
 const Services = () => {
   return (
     <section className='pt-24 pb-16 bg-white' id='services'>
-        <div className="max-w-7 mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className='flex flex-col md:flex-row gap-4 mb-12'>
-                <div>
+                <div className='flex-shrink-0 bg-primary text-black py-2 px-16 rounded-md'>
                     <h2 className='text-2xl font-bold'> Services</h2>
                 </div>
-                <div>
-                    <p className='text-secondary md:w-2/3'></p>
+                <div className='md:w-2/3'>
+                    <p className='text-secondary md:w-2/3'>At our digital marketing agency, we offer a range of services to help business grow and succeed online. 
+                    These services include:</p>
                 </div>
+            </div>
+
+            <div className=''>
+                     {
+                        servicesData.map ((service, index)=>(
+                            <div key={index} className='flex flex-col sm:flex-row md:justify-between 
+                             md:items-center gap-6 p-6 rounded-lg border border-[#191A23]'>
+                                <div className='md:w-1/2 flex flex-col h-full justify-between'>
+                                    <button className={`text-xl py-1.5 font-semibold mb-2 rounded-sm
+                                        sm:w-60 ${service.foregroundColor}`}>{service.title}</button>
+                                        <a href="">
+                                             <img src={LinkIcon} alt=""  className='size-6 rounded-full'/>
+                                        </a>
+                                </div>
+                            </div>
+                        ))
+                     }
             </div>
         </div>
     </section>
